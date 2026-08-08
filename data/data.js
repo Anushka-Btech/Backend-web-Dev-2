09b9baad0150d67e276f7547a21aa8575a65c11a
Anushka <anushkachhoker60@gmail.com>
2026-08-08 12:55:22 +0530

    Initial Commit

--- data/data.js
+++ data/data.js
@@ -1,4 +1,3 @@
-const cities = [
-    "New York","Los Angeles","Chicago","Houston","Phoenix","Philadelphia","San Antonio","San Diego","Dallas","San Jose"
-];
+const cities = ["goa", "delhi", "mumbai", "bangalore", "chennai", "kolkata", "hyderabad", "pune", "jaipur", "lucknow"];
+
 module.exports = cities;
--- index.js
+++ index.js
@@ -8,12 +8,26 @@
 // const filename = path.join(__dirname,"data", 'cities.txt');
 // console.log(filename);
 
-const os = require('os');
-const userInfo = os.userInfo();
-const freeMemory = os.freemem();
-const totalMemory = os.totalmem();
-const uptime = os.uptime();
-console.log("User Info:", userInfo);
-console.log("Free Memory:", freeMemory);
-console.log("Total Memory:", totalMemory);
-console.log("Uptime:", uptime);
+// const os = require('os');
+// const userInfo = os.userInfo();
+// const freeMemory = os.freemem();
+// const totalMemory = os.totalmem();
+// const uptime = os.uptime();
+// console.log("User Info:", userInfo);
+// console.log("Free Memory:", freeMemory);
+// console.log("Total Memory:", totalMemory);
+// console.log("Uptime:", uptime);
+
+
+const chalk = require('chalk');
+console.log(chalk.blue('Hello world!'));
+console.log(chalk.red('Hello world!'));
+console.log(chalk.green('Hello world!'));
+console.log(chalk.yellow('Hello world!'));
+
+
+require("dotenv").config();
+const process = require("process");
+// const name = process.argv[2];
+// console.log(name);
+console.log(process.env.PORT);
